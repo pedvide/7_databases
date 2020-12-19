30a61f06fe61b529f574088f74549140a1acc284
Pedro Villanueva <pedvide@gmail.com>
2020-12-19 05:56:33 +0100

    update

--- postgres/chapter1.sql
+++ postgres/chapter1.sql
@@ -61,3 +61,56 @@ SELECT v.venue_id,
 FROM venues v
     INNER JOIN cities c ON v.postal_code = c.postal_code
     AND v.country_code = c.country_code;
+
+INSERT INTO venues (name, postal_code, country_code)
+VALUES ('Voodoo Doughnut', '97206', 'us')
+RETURNING venue_id;
+
+--- Events
+CREATE TABLE EVENTS (
+    event_id SERIAL PRIMARY KEY,
+    title varchar(255),
+    starts timestamp,
+    ends timestamp,
+    venue_id int,
+    FOREIGN KEY (venue_id) REFERENCES venues (venue_id) MATCH FULL
+);
+
+INSERT INTO EVENTS (title, starts, ends, venue_id)
+VALUES (
+        'Fight Club',
+        '2018-02-15 17:30:00',
+        '2018-02-15 19:30:00',
+        2
+    ),
+    (
+        'April Fools Day',
+        '2018-04-01 00:00:00',
+        '2018-04-01 23:59:00',
+        NULL
+    ),
+    (
+        'Christmas Day',
+        '2018-12-25 00:00:00',
+        '2018-12-25 23:59:00',
+        NULL
+    );
+
+SELECT e.title,
+    v.name
+FROM EVENTS e
+    JOIN venues v ON e.venue_id = v.venue_id;
+
+SELECT e.title,
+    v.name
+FROM EVENTS e
+    LEFT JOIN venues v ON e.venue_id = v.venue_id;
+
+--- indices
+CREATE INDEX events_title ON EVENTS USING hash (title);
+
+SELECT *
+FROM EVENTS
+WHERE starts >= '2018-04-01';
+
+CREATE INDEX events_starts ON EVENTS USING btree (starts);
