f0ae4b065d0e186b07c428ec194d51c6cdec20b8
Pedro Villanueva <pedvide@gmail.com>
2020-12-19 11:38:33 +0100

    add event stored procedure

--- postgres/add_event.sql
+++ postgres/add_event.sql
@@ -1,35 +1,43 @@
 CREATE OR REPLACE FUNCTION add_event(
-  title text,
-  starts timestamp,
-  ends timestamp,
-  venue text,
-  postal varchar(9),
-  country char(2))
-RETURNS boolean AS $$
-DECLARE
-  did_insert boolean := false;
-  found_count integer;
-  the_venue_id integer;
+    title text,
+    starts timestamp,
+    ends timestamp,
+    venue text,
+    postal varchar(9),
+    country char(2)
+  ) RETURNS boolean AS $$
+DECLARE did_insert boolean := false;
+
+found_count integer;
+
+the_venue_id integer;
+
 BEGIN
-  SELECT venue_id INTO the_venue_id
-  FROM venues v
-  WHERE v.postal_code=postal AND v.country_code=country AND v.name ILIKE venue
-  LIMIT 1;
+SELECT venue_id INTO the_venue_id
+FROM venues v
+WHERE v.postal_code = postal
+  AND v.country_code = country
+  AND v.name ILIKE venue
+LIMIT 1;
 
-  IF the_venue_id IS NULL THEN
-    INSERT INTO venues (name, postal_code, country_code)
-    VALUES (venue, postal, country)
-    RETURNING venue_id INTO the_venue_id;
+IF the_venue_id IS NULL THEN
+INSERT INTO venues (name, postal_code, country_code)
+VALUES (venue, postal, country)
+RETURNING venue_id INTO the_venue_id;
 
-    did_insert := true;
-  END IF;
+did_insert := TRUE;
 
-  -- Note: this is a notice, not an error as in some programming languages
-  RAISE NOTICE 'Venue found %', the_venue_id;
+END IF;
 
-  INSERT INTO events (title, starts, ends, venue_id)
-  VALUES (title, starts, ends, the_venue_id);
+-- Note: this is a notice, not an error as in some programming languages
+RAISE NOTICE 'Venue found %',
+the_venue_id;
 
-  RETURN did_insert;
+INSERT INTO EVENTS (title, starts, ends, venue_id)
+VALUES (title, starts, ends, the_venue_id);
+
+RETURN did_insert;
+
 END;
+
 $$ LANGUAGE plpgsql;
