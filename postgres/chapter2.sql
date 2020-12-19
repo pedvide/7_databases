5a70a5272289f7a1b2156da91398bd9d9b8ad7a7
Pedro Villanueva <pedvide@gmail.com>
2020-12-19 11:39:57 +0100

    update

--- postgres/chapter2.sql
+++ postgres/chapter2.sql
@@ -79,3 +79,175 @@ SELECT DISTINCT venue_id
 FROM EVENTS;
 
 --- Window Functions
+SELECT title,
+    COUNT(*) OVER (PARTITION BY venue_id)
+FROM EVENTS;
+
+--- Transactions
+BEGIN TRANSACTION;
+
+DELETE FROM EVENTS;
+
+ROLLBACK;
+
+SELECT *
+FROM EVENTS;
+
+END;
+
+--- Stored Procedures
+SELECT add_event(
+        'House Party',
+        '2018-05-03 23:00',
+        '2018-05-04 02:00',
+        'Run''s House',
+        '97206',
+        'us'
+    );
+
+--- Triggers
+-- Table
+CREATE TABLE LOGS (
+    event_id integer,
+    old_title varchar(255),
+    old_starts timestamp,
+    old_ends timestamp,
+    logged_at timestamp DEFAULT CURRENT_TIMESTAMP
+);
+
+-- stored procedure
+CREATE OR REPLACE FUNCTION log_event() RETURNS TRIGGER AS $$
+DECLARE BEGIN
+INSERT INTO LOGS (event_id, old_title, old_starts, old_ends)
+VALUES (OLD.event_id, OLD.title, OLD.starts, OLD.ends);
+
+RAISE NOTICE 'Someone just changed event #%',
+OLD.event_id;
+
+RETURN NEW;
+
+END;
+
+$$ LANGUAGE plpgsql;
+
+-- Trigger
+CREATE TRIGGER log_events
+AFTER
+UPDATE ON EVENTS FOR EACH ROW EXECUTE PROCEDURE log_event();
+
+-- test trigger
+UPDATE EVENTS
+SET ends = '2018-05-04 01:00:00'
+WHERE title = 'House Party';
+
+SELECT event_id,
+    old_title,
+    old_ends,
+    logged_at
+FROM LOGS;
+
+--- Views
+CREATE VIEW holidays AS
+SELECT event_id AS holiday_id,
+    title AS name,
+    starts AS date
+FROM EVENTS
+WHERE title LIKE '%Day%'
+    AND venue_id IS NULL;
+
+SELECT name,
+    to_char(date, 'Month DD, YYYY') AS date
+FROM holidays
+WHERE date <= '2018-04-01';
+
+ALTER TABLE EVENTS
+ADD colors text ARRAY;
+
+CREATE OR REPLACE VIEW holidays AS
+SELECT event_id AS holiday_id,
+    title AS name,
+    starts AS date,
+    colors
+FROM EVENTS
+WHERE title LIKE '%Day%'
+    AND venue_id IS NULL;
+
+UPDATE holidays
+SET colors = '{"red","green"}'
+WHERE name = 'Christmas Day';
+
+--- Rules
+EXPLAIN VERBOSE
+SELECT *
+FROM holidays;
+
+CREATE RULE update_holidays AS ON UPDATE TO holidays DO INSTEAD
+UPDATE EVENTS
+SET title = NEW.name,
+    starts = NEW.date,
+    colors = NEW.colors
+WHERE title = OLD.name;
+
+UPDATE holidays
+SET colors = '{"red","green"}'
+WHERE name = 'Christmas Day';
+
+--- Crosstab
+SELECT extract(
+        year
+        FROM starts
+    ) AS year,
+    extract(
+        MONTH
+        FROM starts
+    ) AS MONTH,
+    COUNT(*)
+FROM EVENTS
+GROUP BY year,
+    MONTH
+ORDER BY year,
+    MONTH;
+
+CREATE TEMPORARY TABLE month_count(MONTH INT);
+
+INSERT INTO month_count
+VALUES (1),
+    (2),
+    (3),
+    (4),
+    (5),
+    (6),
+    (7),
+    (8),
+    (9),
+    (10),
+    (11),
+    (12);
+
+--- Not in test, needed to use crosstab()
+CREATE EXTENSION IF NOT EXISTS tablefunc;
+
+SELECT *
+FROM crosstab(
+        'SELECT extract(year from starts) as year,
+extract(month from starts) as month, count(*)
+FROM events
+GROUP BY year, month
+ORDER BY year, month',
+        'SELECT * FROM month_count'
+    ) AS (
+        year int,
+        jan int,
+        feb int,
+        mar int,
+        apr int,
+        may int,
+        jun int,
+        jul int,
+        aug int,
+        sep int,
+        oct int,
+        nov int,
+        dec int
+    )
+ORDER BY YEAR;
