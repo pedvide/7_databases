fd81a821d5140a31871568c7865a6dd002073360
Pedro Villanueva <pedvide@gmail.com>
2020-12-20 22:21:52 +0100

    finish homework

--- postgres/chapter2.sql
+++ postgres/chapter2.sql
@@ -251,3 +251,75 @@ ORDER BY year, month',
         dec int
     )
 ORDER BY YEAR;
+
+--- Homework
+-- 1
+CREATE RULE delete_venues AS ON DELETE TO venues DO INSTEAD
+UPDATE venues
+SET active = false
+WHERE venue_id = OLD.venue_id;
+
+SELECT *
+FROM venues;
+
+DELETE FROM venues
+WHERE name = 'My Place';
+
+UPDATE venues
+SET active = TRUE
+WHERE name = 'My Place';
+
+-- 2
+SELECT *
+FROM crosstab(
+        'SELECT extract(year from starts) as year,
+extract(month from starts) as month, count(*)
+FROM events
+GROUP BY year, month
+ORDER BY year, month',
+        'SELECT * FROM generate_series(1, 12)'
+    ) AS (
+        year int,
+        jan int,
+        feb int,
+        mar int,
+        apr int,
+        may int,
+        jun int,
+        jul int,
+        aug int,
+        sep int,
+        oct int,
+        nov int,
+        dec int
+    )
+ORDER BY YEAR;
+
+-- 3
+SELECT week,
+    COALESCE(sunday, 0) AS sunday,
+    COALESCE(monday, 0) AS monday,
+    COALESCE(tuesday, 0) AS tuesday,
+    COALESCE(wednesday, 0) AS wednesday,
+    COALESCE(thursday, 0) AS thursday,
+    COALESCE(friday, 0) AS friday,
+    COALESCE(saturday, 0) AS saturday
+FROM crosstab(
+        'SELECT extract(week from starts) as week,
+extract(dow from starts) as day_of_week, count(*)
+FROM events
+WHERE extract(month from starts) = 2
+GROUP BY week, day_of_week
+ORDER BY week, day_of_week',
+        'SELECT * FROM generate_series(0, 6)'
+    ) AS (
+        week int,
+        sunday int,
+        monday int,
+        tuesday int,
+        wednesday int,
+        thursday int,
+        friday int,
+        saturday int
+    )
+ORDER BY week;
