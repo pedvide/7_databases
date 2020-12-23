367af6e918a3b1e448691d9439edb992efd52963
Pedro Villanueva <pedvide@gmail.com>
2020-12-23 21:41:48 +0100

    start ch 3

---
+++ postgres/chapter3.sql
@@ -0,0 +1,10 @@
+--- Install extensions: tablefunc, dict_xsyn, fuzzystrmatch, pg_trgm, and cube 
+CREATE EXTENSION IF NOT EXISTS tablefunc;
+
+CREATE EXTENSION IF NOT EXISTS dict_xsyn;
+
+CREATE EXTENSION IF NOT EXISTS fuzzystrmatch;
+
+CREATE EXTENSION IF NOT EXISTS pg_trgm;
+
+CREATE EXTENSION IF NOT EXISTS cube;
